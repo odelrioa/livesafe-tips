875fe7ed243035880fd5fae180e6f7104712d92c
Orlando Del Rio <odelrioa@gmail.com>
2021-05-08 20:30:06 -0400

    Modify LegacyTipsService to read from files and return back a list of Tips

--- src/main/scala/com/livesafe/tips/LegacyTipsService.scala
+++ src/main/scala/com/livesafe/tips/LegacyTipsService.scala
@@ -1,5 +1,8 @@
 package com.livesafe.tips
 
+import com.livesafe.tips.models.Tip
+import org.joda.time.DateTime
+
 import scala.concurrent.Future
 import scala.io.Source
 
@@ -7,7 +10,29 @@ object LegacyTipsService {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def fetchLegacy1Tips: Future[String] = Future { Source.fromResource("legacy-1-tips.csv").mkString }
-  def fetchLegacy2Tips: Future[String] = Future { Source.fromResource("legacy-2-tips.csv").mkString }
+  def fetchLegacy1Tips: Future[List[Tip]] = Future {
+    val lines = Source.fromResource("legacy-1-tips.csv").getLines()
+    lines.drop(1).map { line =>
+      val Array(tipId, created, tipType, msg) = line.split(",").map(_.trim)
+      Tip(
+        tipId,
+        created.toLong,
+        tipType,
+        msg
+      )
+    }.toList
+  }
+  def fetchLegacy2Tips: Future[List[Tip]] = Future {
+    val lines = Source.fromResource("legacy-2-tips.csv").getLines()
+    lines.drop(1).map { line =>
+      val Array(tipId, message, createdAt, tipType) = line.split(",").map(_.trim)
+      Tip(
+        tipId,
+        DateTime.parse(createdAt).toInstant.getMillis,
+        tipType,
+        message
+      )
+    }.toList
+  }
 
 }
