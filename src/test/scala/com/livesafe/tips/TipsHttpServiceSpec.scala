dcf041d848d8f8406aeddee79ac028007be6c77a
Orlando Del Rio <odelrioa@gmail.com>
2021-05-09 20:17:34 -0400

    Fix TipsHttpServiceSpec

--- src/test/scala/com/livesafe/tips/TipsHttpServiceSpec.scala
+++ src/test/scala/com/livesafe/tips/TipsHttpServiceSpec.scala
@@ -3,11 +3,13 @@ package com.livesafe.tips
 import java.util.UUID
 
 import scala.concurrent.Future
-
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.livesafe.tips.Service.Tip
+import com.livesafe.tips.models.{Tip, TipType}
+import org.joda.time.DateTime
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{ FreeSpec, Matchers }
+import org.scalatest.{FreeSpec, Matchers}
 
 class TipsHttpServiceSpec extends FreeSpec with Matchers with MockFactory with ScalatestRouteTest {
 
@@ -17,15 +19,22 @@ class TipsHttpServiceSpec extends FreeSpec with Matchers with MockFactory with S
 
   "TipsHttpService endpoints" - {
     "/tips" in {
-      (mockTipService.getTips _).expects().returns(Future.successful(Nil))
+      (mockTipService.getAll _).expects().returns(Future.successful(Nil))
       Get("/tips") ~> service.routes ~> check {
         status shouldBe StatusCodes.OK
       }
     }
     "/tips/:id" in {
       val testTipId = UUID.randomUUID()
-      (mockTipService.getTip _).expects(testTipId).returns(Future.successful("test tip response"))
+      val testTipIdStr = testTipId.toString
+      val tipType = TipType.withName("Disturbance")
+      (mockTipService.getById _).expects(testTipId).returns(Future.successful(Tip(testTipIdStr, DateTime.now(), tipType, "test message")))
       // FIXME: Finish this test please
+      Get(s"/tips/$testTipId") ~> service.routes ~> check {
+        status shouldBe StatusCodes.OK
+        contentType shouldEqual ContentTypes.`application/json`
+        responseAs[String] contains testTipIdStr
+      }
     }
   }
 }
