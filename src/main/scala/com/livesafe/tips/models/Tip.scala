17f92343982a9c28f1e9f4d48fc70880aca62002
Orlando Del Rio <odelrioa@gmail.com>
2021-05-09 20:01:50 -0400

    Modify Tip case class Change field created to be a DateTime Change field `type` to be TipType Add implicit reads/writes for joda DateTime Add implicit reads/writes for object Tip

--- src/main/scala/com/livesafe/tips/models/Tip.scala
+++ src/main/scala/com/livesafe/tips/models/Tip.scala
@@ -1,8 +1,27 @@
 package com.livesafe.tips.models
 
+import com.livesafe.tips.models.TipType.TipType
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import play.api.libs.json._
+
 case class Tip(
                 id: String,
-                created: Long,
-                `type`: String,
+                created: DateTime,
+                `type`: TipType,
                 message: String
               )
+
+object Tip {
+
+  //This implicit for joda DateTimes, could be in a generic Json Support Util to be used across the application
+  //Putting it here because for the exercise purpose
+  implicit val jodaDateTimeReads = Reads[DateTime](js =>
+    js.validate[String].map[DateTime](dtString => DateTime.parse(dtString, DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")))
+  )
+  implicit val jodaDateTimeWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(d: DateTime): JsValue = JsString(d.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")))
+  }
+  implicit val tipWrites = Json.writes[Tip]
+  implicit val tipReads = Json.reads[Tip]
+}
