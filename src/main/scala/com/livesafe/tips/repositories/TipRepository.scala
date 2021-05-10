f73c46deb8939b4f2bebb756e2913b105c283e91
Orlando Del Rio <odelrioa@gmail.com>
2021-05-09 20:00:32 -0400

    Define default constructor for TipRepository to get information from CSV files populatesd to in-memory database

--- src/main/scala/com/livesafe/tips/repositories/TipRepository.scala
+++ src/main/scala/com/livesafe/tips/repositories/TipRepository.scala
@@ -1,6 +1,16 @@
 package com.livesafe.tips.repositories
 
-import com.livesafe.tips.Repository
-import com.livesafe.tips.models.Tip
+import com.livesafe.tips.{LegacyTipsService, Repository}
+import com.livesafe.tips.Service.Tip
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class TipRepository extends Repository[Tip](_.id)
+class TipRepository extends Repository[Tip](_.id) {
+  //As a default constructor this will execute when the repository is instantiated
+  for {
+    legacy1Tips <- LegacyTipsService.fetchLegacy1Tips
+    legacy2Tips <- LegacyTipsService.fetchLegacy2Tips
+  } yield {
+    legacy1Tips.foreach(save(_))
+    legacy2Tips.foreach(save(_))
+  }
+}
