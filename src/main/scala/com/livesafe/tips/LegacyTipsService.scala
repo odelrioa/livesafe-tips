6c668619ec3443a4f2b88988f0994d241da7d0dd
Orlando Del Rio <odelrioa@gmail.com>
2021-05-09 20:05:26 -0400

    Modify LegacyTipsService Get created date as DateTime Get tip type as an Enum

--- src/main/scala/com/livesafe/tips/LegacyTipsService.scala
+++ src/main/scala/com/livesafe/tips/LegacyTipsService.scala
@@ -1,6 +1,6 @@
 package com.livesafe.tips
 
-import com.livesafe.tips.models.Tip
+import com.livesafe.tips.models.{Tip, TipType}
 import org.joda.time.DateTime
 
 import scala.concurrent.Future
@@ -12,24 +12,26 @@ object LegacyTipsService {
 
   def fetchLegacy1Tips: Future[List[Tip]] = Future {
     val lines = Source.fromResource("legacy-1-tips.csv").getLines()
+    //Drops first line because it is the head of the csv
     lines.drop(1).map { line =>
       val Array(tipId, created, tipType, msg) = line.split(",").map(_.trim)
       Tip(
         tipId,
-        created.toLong,
-        tipType,
+        new DateTime(created.toLong), //Assuming that always is a correct TimeStamp (Epoc)
+        TipType.withName(tipType),
         msg
       )
     }.toList
   }
   def fetchLegacy2Tips: Future[List[Tip]] = Future {
     val lines = Source.fromResource("legacy-2-tips.csv").getLines()
+    //Drops first line because it is the head of the csv
     lines.drop(1).map { line =>
       val Array(tipId, message, createdAt, tipType) = line.split(",").map(_.trim)
       Tip(
         tipId,
-        DateTime.parse(createdAt).toInstant.getMillis,
-        tipType,
+        DateTime.parse(createdAt), //Assuming that always is a correct DateTime format
+        TipType.withName(tipType.capitalize),
         message
       )
     }.toList
